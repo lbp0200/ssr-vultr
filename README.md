28e4ef202950d6eb69091d81513b75866915a5e6
liuboping <liuboping@elex-tech.com>
2019-01-20 12:47:26 +0800

    1

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # ssr-vultr
 在vultr上，在线自动部署ssr
+
+>只支持Ubuntu 18.04
+
+目前Vultr在搞活动，新注册用户，充50送25，可以考虑新建的小号薅羊毛，需要老用户邀请，附上邀请链接[https://www.vultr.com/?ref=7780016-4F](https://www.vultr.com/?ref=7780016-4F)
+
+使用方法
+---
+
+### 第一步
+
+打开[Vultr](https://polr.liuboping.com/PrgTf)，登录之后，在`Servers`-`Startup Scripts`-`Add Startup Script`
+
+`name` 随便填
+
+`Type`->`Boot`
+
+最后一项，打开，复制里面的内容，粘贴进去
+
+第二行是参数
+
+PWD填你自己的密码
+
+PORT端口号，不建议修改
+
+最后，点最下方蓝色按钮`Add Script`
+
+### 第二步 部署服务器
+
+新建服务器
+
+`Server Type`选择Ubuntu 18.04
+
+5.`Startup Script`，选中刚才新建的`Startup Script`
+
+> 完成服务器的部署，等待服务器部署完成
+
+效果
+---
+75M宽带
---
+++ VultrStartupScript.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+PWD=pwd
+PORT=443
+
+apt update
+apt install -y curl git python2.7
+
+modprobe tcp_bbr
+echo "tcp_bbr" >> /etc/modules-load.d/modules.conf
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+
+cd /opt
+git clone https://github.com/shadowsocksr-rm/shadowsocksr.git
+
+curl -L -H "Cache-Control: no-cache" -o /opt/shadowsocksr/config.server.json https://raw.githubusercontent.com/lbp0200/ssr-vultr/master/config.json 
+curl -L -H "Cache-Control: no-cache" -o /etc/systemd/system/ssr.service https://raw.githubusercontent.com/lbp0200/ssr-vultr/master/ssr.service
+
+sed -i "s/pwd/${PWD}/g" "/opt/shadowsocksr/config.server.json"
+sed -i "s/443/${PORT}/g" "/opt/shadowsocksr/config.server.json"
+
+systemctl enable ssr.service
+systemctl start ssr.service
