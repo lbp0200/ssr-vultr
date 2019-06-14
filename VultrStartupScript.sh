5fee104c2ec3bc10e144d4c01b4fe1f53497cdf1
John Smith <lbp0200@users.noreply.github.com>
2019-06-14 11:05:32 +0800

    Update VultrStartupScript.sh

--- VultrStartupScript.sh
+++ VultrStartupScript.sh
@@ -7,9 +7,9 @@ apt update
 apt install -y curl git python2.7
 
 modprobe tcp_bbr
-echo "tcp_bbr" >> /etc/modules-load.d/modules.conf
-echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+echo "tcp_bbr" | sudo tee --append /etc/modules-load.d/modules.conf
+echo "net.core.default_qdisc=fq" | sudo tee --append /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" | sudo tee --append /etc/sysctl.conf
 sysctl -p
 
 cd /opt
